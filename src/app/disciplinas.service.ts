9143a4969c95f30191694e6ba001c9752074e020
juliadelua <juliagatoh@gmail.com>
2022-04-01 21:12:17 -0300

    3 files modified

--- src/app/disciplinas.service.ts
+++ src/app/disciplinas.service.ts
@@ -9,7 +9,35 @@ interface Disciplina {
 @Injectable()
 export class DisciplinasService {
   list: Array<Disciplina> = [];
-  constructor() {}
+  constructor() {
+    this.list = [
+      {
+        nome: 'Projeto de Encontrabilidade',
+        diaDaSemana: 'Segunda',
+        horario: '19:00 - 22:30',
+      },
+      {
+        nome: 'Negócios e Marketing e Eletrônicos',
+        diaDaSemana: 'Quarta',
+        horario: '19:00 - 22:30',
+      },
+      {
+        nome: 'Desenvolvimento para Servidores II',
+        diaDaSemana: 'Quinta',
+        horario: '19:00 - 22:30',
+      },
+      {
+        nome: '	Tópicos Especiais em Sistemas para Internet II',
+        diaDaSemana: 'Sexta',
+        horario: '19:00 - 22:30',
+      },
+      {
+        nome: 'Projeto de Prototipagem e Testes de Usabilidade',
+        diaDaSemana: 'Sábado',
+        horario: '8:00 - 11:30',
+      },
+    ];
+  }
 
   getter() {
     return this.list;
